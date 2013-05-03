43bdbb3c465a48fdde3cf270447794408c4a344f
yaowei1188 <yaowei1188@gmail.com>
2013-05-03 13:46:25 +0800

    update ccmesagedialog

--- Classes/CCMessageDialog.cpp
+++ Classes/CCMessageDialog.cpp
@@ -2,26 +2,32 @@
 
 using namespace cocos2d;
 
-CCScene* CCMessageDialog::scene()
+CCMessageDialog::CCMessageDialog()
+: m_pDelegate(NULL)
 {
-    CCScene * scene = NULL;
-    do 
-    {
-        // 'scene' is an autorelease object
-        scene = CCScene::create();
-        CC_BREAK_IF(! scene);
-
-        // 'layer' is an autorelease object
-        CCMessageDialog *layer = CCMessageDialog::create();
-        CC_BREAK_IF(! layer);
-
-        // add layer as a child to scene
-        scene->addChild(layer);
-    } while (0);
-
-    // return the scene
-    return scene;
+    
 }
+
+//CCScene* CCMessageDialog::scene()
+//{
+//    CCScene * scene = NULL;
+//    do 
+//    {
+//        // 'scene' is an autorelease object
+//        scene = CCScene::create();
+//        CC_BREAK_IF(! scene);
+//
+//        // 'layer' is an autorelease object
+//        CCMessageDialog *layer = CCMessageDialog::create();
+//        CC_BREAK_IF(! layer);
+//
+//        // add layer as a child to scene
+//        scene->addChild(layer);
+//    } while (0);
+//
+//    // return the scene
+//    return scene;
+//}
 
 // on "init" you need to initialize your instance
 bool CCMessageDialog::init()
@@ -35,6 +41,8 @@ bool CCMessageDialog::init()
 
         CC_BREAK_IF(! CCLayer::init());
         
+        this->setTouchEnabled(true);
+        
         CCSprite *mBg = CCSprite::createWithSpriteFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("prompt_dialog.png"));
         CCSprite *cancelNormal = CCSprite::createWithSpriteFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("no_1.png"));
         CCSprite *cancelHighlight = CCSprite::createWithSpriteFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("no_2.png"));
@@ -42,11 +50,12 @@ bool CCMessageDialog::init()
         CCSprite *OKHighlight = CCSprite::createWithSpriteFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("ok_2.png"));
 
         CCSize mSize=CCDirector::sharedDirector()->getWinSize();
-        mBg->setPosition(ccp(mSize.width / 2, mSize.height / 2));
+        mBg->setPosition(ccp(mSize.width / 2, mSize.height / 3));
+        mBg->setAnchorPoint(ccp(0.5,0.5));
         this->addChild(mBg);
         
         lblTitle = CCLabelTTF::create("Are you sure ?","Arial",20);
-        lblTitle->setPosition(ccp(mSize.width / 2, mSize.height / 2+40));
+        lblTitle->setPosition(ccp(mSize.width / 2, mSize.height / 3+40));
         this->addChild(lblTitle);
         
         CCMenuItemSprite *pMenuOK = CCMenuItemSprite::create(
@@ -55,7 +64,7 @@ bool CCMessageDialog::init()
                                                              NULL,
                                                              this,
                                                              menu_selector(CCMessageDialog::buttonClicked));
-        pMenuOK->setPosition(ccp(mSize.width *(3.0/10.0), mSize.height / 2-30));
+        pMenuOK->setPosition(ccp(mSize.width *(3.0/10.0), mSize.height / 3-30));
 		pMenuOK->setTag(0);
         
         CCMenuItemSprite *pMenuCancel = CCMenuItemSprite::create(
@@ -64,8 +73,8 @@ bool CCMessageDialog::init()
                                                              NULL,
                                                              this,
                                                              menu_selector(CCMessageDialog::buttonClicked));
-        pMenuCancel->setPosition(ccp(mSize.width *(7.0/10.0), mSize.height / 2-30));
-		pMenuOK->setTag(1);
+        pMenuCancel->setPosition(ccp(mSize.width *(7.0/10.0), mSize.height / 3-30));
+		pMenuCancel->setTag(1);
 
         CCMenu* pMenu = CCMenu::create(pMenuOK,pMenuCancel,NULL);
         pMenu->setPosition(CCPointZero);
@@ -82,9 +91,9 @@ void CCMessageDialog::setTitle(const char * title)
 	lblTitle->setString(title);
 }
 
-void CCMessageDialog::registerWithTouchDispatcher(void){
-
-	CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, INT_MIN , true);
+void CCMessageDialog::registerWithTouchDispatcher(void)
+{
+	CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, 0 , true);
 }
 
 bool CCMessageDialog::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent)
@@ -95,11 +104,10 @@ bool CCMessageDialog::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent)
 void CCMessageDialog::buttonClicked(CCObject *pSender)
 {
 	int tag = ((CCNode *)pSender)->getTag();
-    this->removeFromParentAndCleanup(true);
 	if (m_pDelegate!=NULL)
 	{
 		m_pDelegate->didClickButton(this,tag);
 	}
-
+    this->removeFromParentAndCleanup(true);
 }
 
--- Classes/CCMessageDialog.h
+++ Classes/CCMessageDialog.h
@@ -21,11 +21,12 @@ public:
 class CCMessageDialog : public cocos2d::CCLayer
 {
 public:
+    CCMessageDialog();
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();  
 
-    // there's no 'id' in cpp, so we recommand to return the exactly class pointer
-    static cocos2d::CCScene* scene();
+//    // there's no 'id' in cpp, so we recommand to return the exactly class pointer
+//    static cocos2d::CCScene* scene();
 
     CCB_STATIC_NEW_AUTORELEASE_OBJECT_WITH_INIT_METHOD(CCMessageDialog, create);
     
