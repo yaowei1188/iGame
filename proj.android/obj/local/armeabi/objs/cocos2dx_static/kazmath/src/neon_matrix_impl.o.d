9e41689278faf6a6d54f2207d5426a4fd373b28c
Munex <yaowei1188@gmail.com>
2013-05-18 08:25:44 +0800

    delete android version

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Users/ivan/Documents/ivan/cocos2d-2.1rc0-x-2.1.3/igame/proj.android/../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Users/ivan/Documents/ivan/cocos2d-2.1rc0-x-2.1.3/igame/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/ivan/Documents/ivan/cocos2d-2.1rc0-x-2.1.3/igame/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/ivan/Documents/ivan/cocos2d-2.1rc0-x-2.1.3/igame/proj.android/../../cocos2dx/support/ccUtils.cpp \
- /Users/ivan/Documents/ivan/cocos2d-2.1rc0-x-2.1.3/igame/proj.android/../../cocos2dx/support/ccUtils.h
-
-/Users/ivan/Documents/ivan/cocos2d-2.1rc0-x-2.1.3/igame/proj.android/../../cocos2dx/support/ccUtils.h:
